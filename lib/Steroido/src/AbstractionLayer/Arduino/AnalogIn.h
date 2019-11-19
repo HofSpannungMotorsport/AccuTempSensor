f482a89f5ed49d40bb04a65005adb79bd90e3286
Timo <timoxd7@gmx.de>
2019-11-19 01:37:40 +0100

    Add temporarily version of Steroido OS

---
+++ lib/Steroido/src/AbstractionLayer/Arduino/AnalogIn.h
@@ -0,0 +1,36 @@
+#ifndef ANALOG_IN_H
+#define ANALOG_IN_H
+
+#ifndef ANALOG_IN_RESOLUTION
+    #define ANALOG_IN_RESOLUTION 10 // bit -> has to be at least 10
+#endif
+
+class AnalogIn : private NonCopyable<AnalogIn> {
+    public:
+        AnalogIn(PinName pin)
+        : _pin(pin) {
+            pinMode(pin, INPUT);
+            _devider = (1 << ANALOG_IN_RESOLUTION) - 1;
+        }
+
+        float read() {
+            return (float)analogRead(_pin) / (float)_devider;
+        }
+
+        uint16_t read_u16() {
+            uint16_t value = (analogRead(_pin) << (16 - ANALOG_IN_RESOLUTION));
+            value |= (value >> ANALOG_IN_RESOLUTION);
+
+            return value;
+        }
+
+        operator float() {
+            return read();
+        }
+
+    private:
+        PinName _pin;
+        uint16_t _devider;
+};
+
+#endif // ANALOG_IN_H
---
+++ lib/Steroido/src/Steroido.h
@@ -0,0 +1,41 @@
+#ifndef STEROIDO_H
+#define STEROIDO_H
+
+#include "Common/freeMemory.h"
+#include "Common/memCpy.h"
+#include "Common/memSet.h"
+
+
+// Include Arduino Hardware Drivers
+#ifdef Arduino_h
+    // STL
+    #include "Common/vector.h"
+
+    // Abstraction Layer
+    #include "AbstractionLayer/Arduino/printfIntegration.h"
+    #include "AbstractionLayer/Arduino/NonCopyable.h"
+    #include "AbstractionLayer/Arduino/Callback.h"
+    #include "AbstractionLayer/Arduino/CircularBuffer.h"
+    #include "AbstractionLayer/Arduino/Timer.h"
+    #include "AbstractionLayer/Arduino/PinName.h"
+    #include "AbstractionLayer/Arduino/PinMode.h"
+    #include "AbstractionLayer/Arduino/AnalogIn.h"
+    #include "AbstractionLayer/Arduino/AnalogOut.h"
+    #include "AbstractionLayer/Arduino/DigitalIn.h"
+    #include "AbstractionLayer/Arduino/DigitalOut.h"
+    #include "AbstractionLayer/Arduino/PwmOut.h"
+#endif // Arduino_h
+
+
+#ifdef TEENSY
+    #include "AbstractionLayer/Teensyduino/CAN.h"
+#endif // TEENSY
+
+
+#ifdef MBED_H
+    // Include nothing, brings all with it
+    #define VECTOR_EMPLACE_BACK_ENABLED
+#endif // MBED_H
+
+
+#endif // STEROIDO_H
